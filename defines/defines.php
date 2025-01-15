308f0bcbebf79c395e78e2e29330dd69ace763b2
BrianAbura <byaruhanga.abu@gmail.com>
2025-01-15 09:57:45 +0300

    defines updated

--- defines/defines.php
+++ defines/defines.php
@@ -3,17 +3,17 @@ date_default_timezone_set('Africa/Nairobi');
 require_once('meekrodb.2.3.class.php');
 require_once('Logger.php');
 
-define( 'LOG_FILE', 'egpSacco.log');
-define( 'DB_HOST', 'localhost' );
-define( 'DB_USER', 'root' );
-define( 'DB_PASS', 'MSql@db24' ); 
-define( 'DB_NAME', 'egpsavings' );
+define('LOG_FILE', '/var/log/egpSacco/egpSacco.log');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'AburaDB');
+define('DB_PASS', '24@DBpass73');
+define('DB_NAME', 'egpsavings');
 
 /** SMS */
-define( 'SMS_URL', 'https://www.lyptustech.com/lyptus-api/sms/' );
-define( 'SMS_USER', 'EGIC-SMS-21002-EGIC2021' );
-define( 'SMS_PASS', 'EGIC-200-222345-SMS2021' );
-define( 'CLIENT_ID', '82354' );
+define('SMS_URL', 'https://www.lyptustech.com/lyptus-api/sms/');
+define('SMS_USER', 'EGIC-SMS-21002-EGIC2021');
+define('SMS_PASS', 'EGIC-200-222345-SMS2021');
+define('CLIENT_ID', '82354');
 
 /** EMAIL */
 #define( 'EMAIL_URL', 'https://www.lyptustech.com/lyptus-api/email/' );
@@ -24,4 +24,3 @@ DB::$user = DB_USER;
 DB::$password = DB_PASS;
 DB::$dbName = DB_NAME;
 DB::$host = DB_HOST;
-?>
