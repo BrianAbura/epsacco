63b0c8cad26345647c21dbd0c29e619251fda718
BrianAbura <byaruhanga.abu@gmail.com>
2025-03-13 15:43:01 +0300

    Notifications on Loan approval and Guarantor action

--- members/guarantorAction.php
+++ members/guarantorAction.php
@@ -6,11 +6,29 @@ $GuarantorId = htmlspecialchars(( isset( $_REQUEST['GuarantorId'] ) )?  $_REQUES
 $Status = htmlspecialchars(( isset( $_REQUEST['Status'] ) )?  $_REQUEST['Status']: null);
 $Comments = htmlspecialchars(( isset( $_REQUEST['Comments'] ) )?  $_REQUEST['Comments']: null);
 
+$guarantor_request = DB::queryFirstRow('SELECT * from guarantors where Id=%s', $GuarantorId);
+$loan = DB::queryFirstRow('SELECT * from loanrequests where LoanId=%s', $guarantor_request['LoanId']);
+
+$guarantor = DB::queryFirstRow('SELECT * from members where Id=%s', $guarantor_request['AccNumber']);
+$member = DB::queryFirstRow('SELECT * from members where AccNumber=%s', $loan['AccNumber']);
+
     $GurantAction = array(
     'Status'=>$Status,
     'Comments'=>$Comments,
     );
 
     DB::update('guarantors', $GurantAction, 'Id=%s', $GuarantorId);
+
+    // Send Notification
+    if ($Status == "Accepted") {
+        $TableId = "LR - " . $loan['Id'];
+        $SMS = "Dear ".$member['Name'].", ".$guarantor['Name']." has accepted to guarantee your e-GP Investment Club loan of UGX" . number_format($loan['Principal']);
+        SendSms(formatNumber($member['MSISDN']), $SMS, $TableId, "SYSTEM");
+    } else {
+        $TableId = "LR - " . $loan['Id'];      
+        $SMS = "Dear ".$member['Name'].", ".$guarantor['Name']." has declined to guarantee your e-GP Investment Club loan of UGX" . number_format($loan['Principal']);
+        SendSms(formatNumber($member['MSISDN']), $SMS, $TableId, "SYSTEM");
+    }
+    
     echo "Your response has been successfully captured."
 ?>
--- scripts/LoanApprovals.php
+++ scripts/LoanApprovals.php
@@ -1,79 +1,88 @@
-<?php 
+<?php
 require_once('../defines/functions.php');
 require_once('../validate.php');
 
 $CreatedBy = CreatedBy($_SESSION['AccId']);
 
-$LoanId = ( isset( $_REQUEST['LoanId'] ) )?  $_REQUEST['LoanId']: null;
-$Status = ( isset( $_REQUEST['Status'] ) )?  $_REQUEST['Status']: null;
-$Reason = ( isset( $_REQUEST['Reason'] ) )?  $_REQUEST['Reason']: null;
+$LoanId = (isset($_REQUEST['LoanId'])) ?  $_REQUEST['LoanId'] : null;
+$Status = (isset($_REQUEST['Status'])) ?  $_REQUEST['Status'] : null;
+$Reason = (isset($_REQUEST['Reason'])) ?  $_REQUEST['Reason'] : null;
 $dateUpdated = date('Y-m-d H:i:s');
 
 $loan = DB::queryFirstRow('SELECT * from loanrequests where LoanId=%s', $LoanId);
 
-if(!$loan){
+if (!$loan) {
 	echo "This loan does not exist or has already been approved. Please contact the Administrator for assistance.";
-}
-else{
+} else {
 	$member = DB::queryFirstRow('SELECT * from members where AccNumber=%s', $loan['AccNumber']);
 	$approval = DB::queryFirstRow('SELECT * from loanapprovals where LoanId=%s order by Id desc', $loan['LoanId']);
 	//Update Approvals based on the Action
-	DB::update('loanapprovals', array('ReviewBy'=>$CreatedBy, 'Status'=>$Status, 'Narration'=>$Reason, 'Date'=>$dateUpdated), 'Id=%s', $approval['Id']);
+	DB::update('loanapprovals', array('ReviewBy' => $CreatedBy, 'Status' => $Status, 'Narration' => $Reason, 'Date' => $dateUpdated), 'Id=%s', $approval['Id']);
 	// Approval flow: HoF(2) >> Chairperson(1) >> Treasurer(3)
-	if($approval['RoleId'] == 3){ //HOF Level
-		if($Status == "APPROVED"){
-			DB::update('loanapprovals', array('ReviewBy'=>$CreatedBy, 'Status'=>"COMPLETED", 'Narration'=>$Reason, 'Date'=>$dateUpdated), 'Id=%s', $approval['Id']);
-			$DueDate = date('Y-m-d h:i:s', strtotime(date('Y-m-d h:i:s'). ' + 1 months'));
+	if ($approval['RoleId'] == 3) { //HOF Level
+		if ($Status == "APPROVED") {
+			DB::update('loanapprovals', array('ReviewBy' => $CreatedBy, 'Status' => "COMPLETED", 'Narration' => $Reason, 'Date' => $dateUpdated), 'Id=%s', $approval['Id']);
+			$DueDate = date('Y-m-d h:i:s', strtotime(date('Y-m-d h:i:s') . ' + 1 months'));
 			$UpdateLoan = array(
-			"Status"=>"OUTSTANDING",
-			"DueDate"=>$DueDate,
-			);
-			DB::update('loanrequests', $UpdateLoan, 'LoanId=%s', $LoanId);
-	
-			//Update the Guarantors
-			DB::update('guarantors', array('LoanStatus'=>'OUTSTANDING'), 'LoanId=%s', $LoanId);
-	
-			//Add Interests
-			$Interests = array(
-			'LoanId'=>$LoanId,
-			'AccNumber'=>$loan['AccNumber'],
-			'Amount'=>$loan['Interest'],
-			'CreatedBy'=>$CreatedBy,
-			);
-			DB::insert('interests', $Interests);
-	
-			//Record this History
-			$LoanHistory = array(
-			'LoanId'=>$LoanId,
-			'TransactionType'=>'Loan Approved',
-			'Amount'=>$loan['TotalAmount'],
-			'AddedBy'=>$CreatedBy,
-			);
-			DB::insert('loanhistory', $LoanHistory);
-			echo $member['Name']."'s loan of UGX".number_format($loan['Principal'])." has been fully approved and disbursed."; 
-		}
-		else{
-			$UpdateLoan = array(
-			"Status"=>"REJECTED",
-			"ApprovalStatus"=>"Rejected at Treasury",
-			"ApprovedBy"=>$CreatedBy,
-			"ApprovalReason"=>$Reason,
+				"Status" => "OUTSTANDING",
+				"DueDate" => $DueDate,
 			);
 			DB::update('loanrequests', $UpdateLoan, 'LoanId=%s', $LoanId);
 
 			//Update the Guarantors
-			DB::update('guarantors', array('LoanStatus'=>'REJECTED'), 'LoanId=%s', $LoanId);
+			DB::update('guarantors', array('LoanStatus' => 'OUTSTANDING'), 'LoanId=%s', $LoanId);
+
+			//Add Interests
+			$Interests = array(
+				'LoanId' => $LoanId,
+				'AccNumber' => $loan['AccNumber'],
+				'Amount' => $loan['Interest'],
+				'CreatedBy' => $CreatedBy,
+			);
+			DB::insert('interests', $Interests);
+
+			//Record this History
+			$LoanHistory = array(
+				'LoanId' => $LoanId,
+				'TransactionType' => 'Loan Approved',
+				'Amount' => $loan['TotalAmount'],
+				'AddedBy' => $CreatedBy,
+			);
+			DB::insert('loanhistory', $LoanHistory);
+
+			// Send Notification
+			$TableId = "LR - " . $loan['Id'];
+			$SMS = "Your e-GP Investment Club Loan of UGX" . number_format($loan['Principal']) . " has been Approved.";
+			SendSms(formatNumber($member['MSISDN']), $SMS, $TableId, "SYSTEM");
+
+			echo $member['Name'] . "'s loan of UGX" . number_format($loan['Principal']) . " has been fully approved and disbursed.";
+		} else {
+			$UpdateLoan = array(
+				"Status" => "REJECTED",
+				"ApprovalStatus" => "Rejected at Treasury",
+				"ApprovedBy" => $CreatedBy,
+				"ApprovalReason" => $Reason,
+			);
+			DB::update('loanrequests', $UpdateLoan, 'LoanId=%s', $LoanId);
+
+			//Update the Guarantors
+			DB::update('guarantors', array('LoanStatus' => 'REJECTED'), 'LoanId=%s', $LoanId);
 
 			//Loan History
 			$LoanHistory = array(
-			'LoanId'=>$LoanId,
-			'TransactionType'=>'Loan Rejected',
-			'Amount'=>$loan['TotalAmount'],
-			'AddedBy'=>$CreatedBy,
+				'LoanId' => $LoanId,
+				'TransactionType' => 'Loan Rejected',
+				'Amount' => $loan['TotalAmount'],
+				'AddedBy' => $CreatedBy,
 			);
 			DB::insert('loanhistory', $LoanHistory);
-			echo "You have Rejected ".$member['Name']."'s Loan of UGX".number_format($loan['Principal'])."<br/><br/>Reason: ".$Reason;
+
+			// Send Notification
+			$TableId = "LR - " . $loan['Id'];
+			$SMS = "Your e-GP Investment Club Loan of UGX" . number_format($loan['Principal']) . " has been Rejected. Reason: " . $Reason;
+			SendSms(formatNumber($member['MSISDN']), $SMS, $TableId, "SYSTEM");
+
+			echo "You have Rejected " . $member['Name'] . "'s Loan of UGX" . number_format($loan['Principal']) . "<br/><br/>Reason: " . $Reason;
 		}
 	}
 }
-?>
